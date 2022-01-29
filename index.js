50f3d6599bbce5837f4c410b92848a337fdfc23d
Syful Islam Sharif <sharif.syful@gmail.com>
2022-01-29 20:27:41 +0600

    logic trial

--- index.js
+++ index.js
@@ -11,83 +11,102 @@ let baseSubsidyD2D = 5;
 let subsidy;
 // console.log(userChoice);
 
-function subsidyCalculation (userChoice, distanceValue) {
-    switch (userChoice == "1") {
+function subsidyCalculation (choice, distance) {
+    
+    switch (choice == "1") {
         case distance <= 50:
-            subsidy = baseSubsidyP2D + (distanceValue * 0.2);
+            subsidy = baseSubsidyP2D + (distance * 0.2);
             break;
-        case distance <= 100:
-            subsidy = baseSubsidyP2D + (distanceValue* 0.18);
+        case distance > 50 && distance <101:
+            subsidy = baseSubsidyP2D + (distance * 0.18);
             break;
-        case distance <= 150:
-            subsidy = baseSubsidyP2D + (distanceValue * 0.17);
+        case distance > 100 && distance < 151:
+            subsidy = baseSubsidyP2D + (distance * 0.17);
             break;
-        case distance <= 200:
-            subsidy = baseSubsidyP2D + (distanceValue * 0.15);
+        case distance > 150 && distance < 201:
+            subsidy = baseSubsidyP2D + (distance * 0.15);
             break;
-        case distance <= 250:
-            subsidy = baseSubsidyP2D + (distanceValue * 0.14);
+        case distance > 200 && distance < 251:
+            subsidy = baseSubsidyP2D + (distance * 0.14);
             break;
-        case distance <= 300:
-            subsidy = baseSubsidyP2D + (distanceValue* 0.13);
+        case distance > 250 && distance < 301:
+            subsidy = baseSubsidyP2D + (distance * 0.13);
             break;
-        case distance <= 400:
-            subsidy = baseSubsidyP2D + (distanceValue * 0.13);
+        case distance > 300 && distance < 351:
+            subsidy = baseSubsidyP2D + (distance * 0.13);
             break;
-        case distance >= 500:
-            subsidy = baseSubsidyP2D + (distanceValue * 0.12);
+        case distance > 350 && distance < 401:
+            subsidy = baseSubsidyP2D + (distance* 0.12);
+            break;
+        case distance > 400 && distance < 501:
+            subsidy = baseSubsidyP2D + (distance * 0.10);
+            break;
+        case distance > 500:
+            subsidy = baseSubsidyP2D + (distance * 0.10);
             break;
     }
-    switch (userChoice == "2") {
+    switch (choice == "2") {
         case distance <= 50:
-            subsidy = baseSubsidyP2Depot + (distanceValue * 0.15);
+            subsidy = baseSubsidyP2Depot + (distance * 0.15);
             break;
-        case distance <= 100:
-            subsidy = baseSubsidyP2Depot + (distanceValue * 0.13);
+        case distance > 50 && distance <101:
+            subsidy = baseSubsidyP2Depot + (distance * 0.13);
             break;
-        case distance <= 150:
-            subsidy = baseSubsidyP2Depot + (distanceValue* 0.12);
+        case distance > 100 && distance < 151:
+            subsidy = baseSubsidyP2Depot + (distance * 0.12);
             break;
-        case distance <= 200:
-            subsidy = baseSubsidyP2Depot + (distanceValue* 0.11);
+        case distance > 150 && distance < 201:
+            subsidy = baseSubsidyP2Depot + (distance * 0.11);
             break;
-        case distance <= 250:
-            subsidy = baseSubsidyP2Depot + (distanceValue* 0.10);
+        case distance > 200 && distance < 251:
+            subsidy = baseSubsidyP2Depot + (distance * 0.10);
             break;
-        case distance <= 300:
-            subsidy = baseSubsidyP2Depot + (distanceValue* 0.09);
+        case distance > 250 && distance < 301:
+            subsidy = baseSubsidyP2Depot + (distance * 0.09);
             break;
-        case distance <= 400:
-            subsidy = baseSubsidyP2Depot + (distanceValue* 0.08);
+        case distance > 300 && distance < 351:
+            subsidy = baseSubsidyP2Depot + (distance * 0.08);
             break;
-        case distance >= 500:
-            subsidy = baseSubsidyP2Depot + (distanceValue* 0.07);
+        case distance > 350 && distance < 401:
+            subsidy = baseSubsidyP2Depot + (distance* 0.08);
+            break;
+        case distance > 400 && distance < 501:
+            subsidy = baseSubsidyP2Depot + (distance * 0.07);
+            break;
+        case distance > 500:
+            subsidy = baseSubsidyP2Depot + (distance * 0.07);
             break;
     }
-    switch (userChoice == "3") {
+    switch (choice == "3") {
         case distance <= 50:
-            subsidy = baseSubsidyD2D+ (distanceValue * 0.15);
+            subsidy = baseSubsidyD2D + (distance * 0.15);
             break;
-        case distance <= 100:
-            subsidy = baseSubsidyD2D + (distanceValue* 0.13);
+        case distance > 50 && distance <101:
+            subsidy = baseSubsidyD2D+ (distance * 0.13);
             break;
-        case distance <= 150:
-            subsidy = baseSubsidyD2D + (distanceValue* 0.12);
+        case distance > 100 && distance < 151:
+            subsidy = baseSubsidyD2D+ (distance * 0.12);
             break;
-        case distance <= 200:
-            subsidy = baseSubsidyD2D + (distanceValue* 0.11);
+        case distance > 150 && distance < 201:
+            subsidy = baseSubsidyD2D + (distance * 0.11);
             break;
-        case distance <= 250:
-            subsidy = baseSubsidyD2D + (distanceValue* 0.10);
+        case distance > 200 && distance < 251:
+            subsidy = baseSubsidyD2D + (distance * 0.10);
             break;
-        case distance <= 300:
-            subsidy = baseSubsidyD2D + (distanceValue * 0.09);
+        case distance > 250 && distance < 301:
+            subsidy = baseSubsidyD2D + (distance * 0.09);
             break;
-        case distance <= 400:
-            subsidy = baseSubsidyD2D + (distanceValue * 0.08);
+        case distance > 300 && distance < 351:
+            subsidy = baseSubsidyD2D+ (distance * 0.08);
             break;
-        case distance >= 500:
-            subsidy = baseSubsidyD2D + (distanceValue * 0.07);
+        case distance > 350 && distance < 401:
+            subsidy = baseSubsidyD2D + (distance* 0.08);
+            break;
+        case distance > 400 && distance < 501:
+            subsidy = baseSubsidyD2D + (distance * 0.07);
+            break;
+        case distance > 500:
+            subsidy = baseSubsidyD2D + (distance * 0.07);
             break;
     }
 }
@@ -97,8 +116,9 @@ calculateButton.onclick = function showResult() {
     let userChoice = distributionMode.value;
     subsidyCalculation(userChoice, distanceValue);
     resultP.innerHTML = `<p class="fw-bold mt-5 px-5">Total Subsidy Amount ${subsidy}</p>`;
-    // console.log(subsidy);
-    // console.log(distanceValue);
+    console.log(userChoice);
+    console.log(distanceValue);
+    console.log(subsidy);
 
 }
 
